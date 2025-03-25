3f5962c8fdca9f5e0ef4655935ea420262bf501b
LuckyJames128 <166640136+LuckyJames128@users.noreply.github.com>
2025-03-25 10:11:08 -0700

    Add files via upload
    Signed-off-by: LuckyJames128 <166640136+LuckyJames128@users.noreply.github.com>

--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var speed = 200
 var timer_check = false
 var impatient_on = false
+@onready var can_move = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,55 +17,61 @@ func action() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("dialogue"):
-		var actionables = $actionable_finder.get_overlapping_areas()
-		if actionables.size() > 0:
-			actionables[0].action()
-			return
-		else:
-			pass
-	
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+	# if can_move == true:
+		var velocity = Vector2.ZERO # The player's movement vector.
+		
+		if Input.is_action_pressed("move_right"):
+			velocity.x += 1
+		if Input.is_action_pressed("move_left"):
+			velocity.x -= 1
+		if Input.is_action_pressed("move_down"):
+			velocity.y += 1
+		if Input.is_action_pressed("move_up"):
+			velocity.y -= 1
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-		impatient_on = false
-		timer_check = false
-	else:
-		$AnimatedSprite2D.play()
-	
-	position += velocity * delta
-	
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "run_side"
-		$AnimatedSprite2D.flip_v = false
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y > 0:
-		$AnimatedSprite2D.animation = "run_down"
-	elif velocity.y < 0:
-		$AnimatedSprite2D.animation = "run_up"
-	
-	if velocity.length() == 0:
-		if timer_check == false:
-			$idle_timer.start()
-			timer_check = true
+		if velocity.length() > 0:
+			velocity = velocity.normalized() * speed
+			$AnimatedSprite2D.play()
+			impatient_on = false
+			timer_check = false
+		else:
+			$AnimatedSprite2D.play()
 		
-		if impatient_on == false:
-			$AnimatedSprite2D.animation = "idle"
+		position += velocity * delta
+		
+		if velocity.x != 0:
+			$AnimatedSprite2D.animation = "run_side"
 			$AnimatedSprite2D.flip_v = false
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+		elif velocity.y > 0:
+			$AnimatedSprite2D.animation = "run_down"
+		elif velocity.y < 0:
+			$AnimatedSprite2D.animation = "run_up"
 		
-	move_and_slide()
+		if velocity.length() == 0:
+			if timer_check == false:
+				$idle_timer.start()
+				timer_check = true
+			
+			if impatient_on == false:
+				$AnimatedSprite2D.animation = "idle"
+				$AnimatedSprite2D.flip_v = false
+			
+		move_and_slide()
+	#else: pass
 
 func _on_idle_timer_timeout():
 	$AnimatedSprite2D.animation = "impatient_idle"
 	impatient_on = true
 	
+
+
+func _on_game_state_dialogue_start():
+	print("start2")
+	await get_tree().create_timer(1.0).timeout
+	$player.paused = true
+
+
+func _on_game_state_dialogue_end():
+	print("end2")
+	$player.paused = false
