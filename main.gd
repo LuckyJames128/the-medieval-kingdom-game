a38ef2c4331ef77d6cc433d78cbd4137e7c335c4
LuckyJames128 <166640136+LuckyJames128@users.noreply.github.com>
2025-03-26 09:44:16 -0700

    Add files via upload
    Signed-off-by: LuckyJames128 <166640136+LuckyJames128@users.noreply.github.com>

--- main.gd
+++ main.gd
@@ -1,48 +1,14 @@
 extends Node
 # Called when the node enters the scene tree for the first time.
-
-
-@export var dialogue_resource: DialogueResource
-@export var dialogue_start: String = "Start"
-
-func action() -> void:
-	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
-
-
 func _ready() -> void:
 	pass # Replace with function body.
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if Input.is_action_pressed("dialogue"):
-		var actionables = $TileMap/player/actionable_finder.get_overlapping_bodies()
-		if actionables.size() > 0:
-			actionables[0].action()
-			return
-
 	$TileMap/NPC_Viking_Leader/AnimatedSprite2D.flip_h = $TileMap/player.position.x < $TileMap/NPC_Viking_Leader.position.x
+	$TileMap/NPC_Viking_1/AnimatedSprite2D.flip_h = $TileMap/player.position.x < $TileMap/NPC_Viking_1.position.x
+	$TileMap/NPC_Viking_2/AnimatedSprite2D.flip_h = $TileMap/player.position.x < $TileMap/NPC_Viking_2.position.x
+	$TileMap/NPC_Viking_3/AnimatedSprite2D.flip_h = $TileMap/player.position.x < $TileMap/NPC_Viking_3.position.x
+	$TileMap/NPC_Fishing_Leader/AnimatedSprite2D.flip_h = $TileMap/player.position.x < $TileMap/NPC_Fishing_Leader.position.x
+	$TileMap/NPC_Fishing_1/AnimatedSprite2D.flip_h = $TileMap/player.position.x < $TileMap/NPC_Fishing_1.position.x
 	
-	if $TileMap/player.position.x < $TileMap/NPC_Viking_1.position.x:
-		$TileMap/NPC_Viking_1/AnimatedSprite2D.flip_h = true
-	else:
-		$TileMap/NPC_Viking_1/AnimatedSprite2D.flip_h = false
 	
-	if $TileMap/player.position.x < $TileMap/NPC_Viking_2.position.x:
-		$TileMap/NPC_Viking_2/AnimatedSprite2D.flip_h = true
-	else:
-		$TileMap/NPC_Viking_2/AnimatedSprite2D.flip_h = false
-
-	if $TileMap/player.position.x < $TileMap/NPC_Viking_3.position.x:
-		$TileMap/NPC_Viking_3/AnimatedSprite2D.flip_h = true
-	else:
-		$TileMap/NPC_Viking_3/AnimatedSprite2D.flip_h = false
-
-
-	if $TileMap/player.position.x < $TileMap/NPC_Fishing_Leader.position.x:
-		$TileMap/NPC_Fishing_Leader/AnimatedSprite2D.flip_h = true
-	else:
-		$TileMap/NPC_Fishing_Leader/AnimatedSprite2D.flip_h = false
-
-	if $TileMap/player.position.x < $TileMap/NPC_Fishing_1.position.x:
-		$TileMap/NPC_Fishing_1/AnimatedSprite2D.flip_h = true
-	else:
-		$TileMap/NPC_Fishing_1/AnimatedSprite2D.flip_h = false
--- player.gd
+++ player.gd
@@ -3,23 +3,23 @@ extends CharacterBody2D
 @export var speed = 200
 var timer_check = false
 var impatient_on = false
-@onready var can_move = true
 
+@onready var can_move = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-@export var dialogue_resource: DialogueResource
-@export var dialogue_start: String = "Start"
-
-func action() -> void:
-	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# if can_move == true:
-		var velocity = Vector2.ZERO # The player's movement vector.
-		
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("dialogue"):
+		var actionables = $actionable_finder.get_overlapping_areas()
+		if actionables.size() > 0:
+			actionables[0].action()
+			return
+	
+	if can_move == true:
 		if Input.is_action_pressed("move_right"):
 			velocity.x += 1
 		if Input.is_action_pressed("move_left"):
@@ -29,49 +29,50 @@ func _process(delta):
 		if Input.is_action_pressed("move_up"):
 			velocity.y -= 1
 
-		if velocity.length() > 0:
-			velocity = velocity.normalized() * speed
-			$AnimatedSprite2D.play()
-			impatient_on = false
-			timer_check = false
-		else:
-			$AnimatedSprite2D.play()
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+		impatient_on = false
+		timer_check = false
+	else:
+		$AnimatedSprite2D.play()
+	
+	position += velocity * delta
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "run_side"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y > 0:
+		$AnimatedSprite2D.animation = "run_down"
+	elif velocity.y < 0:
+		$AnimatedSprite2D.animation = "run_up"
+	
+	if velocity.length() == 0:
+		if timer_check == false:
+			$idle_timer.start()
+			timer_check = true
 		
-		position += velocity * delta
-		
-		if velocity.x != 0:
-			$AnimatedSprite2D.animation = "run_side"
+		if impatient_on == false:
+			$AnimatedSprite2D.animation = "idle"
 			$AnimatedSprite2D.flip_v = false
-			$AnimatedSprite2D.flip_h = velocity.x < 0
-		elif velocity.y > 0:
-			$AnimatedSprite2D.animation = "run_down"
-		elif velocity.y < 0:
-			$AnimatedSprite2D.animation = "run_up"
 		
-		if velocity.length() == 0:
-			if timer_check == false:
-				$idle_timer.start()
-				timer_check = true
-			
-			if impatient_on == false:
-				$AnimatedSprite2D.animation = "idle"
-				$AnimatedSprite2D.flip_v = false
-			
-		move_and_slide()
-	#else: pass
+	move_and_slide()
+	if can_move == false:
+		print("IM NOT MOVING")
+	if can_move == true:
+		print("IM MOVING")
 
 func _on_idle_timer_timeout():
 	$AnimatedSprite2D.animation = "impatient_idle"
 	impatient_on = true
 	
 
-
-func _on_game_state_dialogue_start():
-	print("start2")
-	await get_tree().create_timer(1.0).timeout
-	$player.paused = true
+func _on_state_dialogue_ended() -> void:
+	print("stop")
+	can_move = false
 
 
-func _on_game_state_dialogue_end():
-	print("end2")
-	$player.paused = false
+func _on_state_dialogue_started() -> void:
+	print("start")
+	can_move = true
